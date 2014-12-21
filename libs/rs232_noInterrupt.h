6f6d7df8cc599e83fa1f9e2096aba6f17e68a281
Scheik <Scheik@users.noreply.github.com>
2014-12-21 13:39:17 +0100

    Update rs232_noInterrupt.h: Change baudrate from 38400 to 9600bps

--- libs/rs232_noInterrupt.h
+++ libs/rs232_noInterrupt.h
@@ -11,23 +11,23 @@
 
 /* Schnittstellenparameter              */
 /****************************************/
-#define UART_BUFFER_SIZE 16													// UART-Puffer- Größe, in Bytes
-#define UART_BAUDRATE 38400UL												// RS232 Baudrate
+#define UART_BUFFER_SIZE 16													// UART-Puffer- GrÃ¶ÃŸe, in Bytes
+#define UART_BAUDRATE 9600UL												// RS232 Baudrate
 #define BAUD_PRESCALE (((F_CPU / (UART_BAUDRATE * 16UL))) - 1)
 #define UBRR_VAL ((F_CPU+UART_BAUDRATE*8)/(UART_BAUDRATE*16)-1)				// runden
 #define BAUD_REAL (F_CPU/(16*(UBRR_VAL+1)))									// Reale Baudrate bei gegebener Taktfrequenz (FCPU) ermitteln
 #define BAUD_ERROR ((BAUD_REAL*1000)/UART_BAUDRATE)							// Fehler in Promille ermitteln
 #if ((BAUD_ERROR<990) || (BAUD_ERROR>1010))
-	#error Systematischer Fehler der Baudrate grösser 1% und damit zu hoch! // Ist der Fehler > 1% gibt der Compiler einen Fehler aus
+	#error Systematischer Fehler der Baudrate grÃ¶sser 1% und damit zu hoch! // Ist der Fehler > 1% gibt der Compiler einen Fehler aus
 #endif
-#define CR "\r\n"															// Ein Zeilenumbruch, abhängig davon, was die Gegenstelle haben will, hier für Windows : "\r\n"
+#define CR "\r\n"															// Ein Zeilenumbruch, abhÃ¤ngig davon, was die Gegenstelle haben will, hier fÃ¼r Windows : "\r\n"
 																			// Linux  : "n"
 																			// MacOS  : "r"
 /* Funktionen und Variablen in rs232.c */
 /***************************************/
 //extern volatile uint8_t UART_MSG_FLAG;										// Variable UART_MSG_FLAG=1, wenn letzes empfangenes Zeichen ein Zeilenumbruch war oder UART_Puffer voll ist
-//extern volatile unsigned char UART_RXBuffer[UART_BUFFER_SIZE];				// Ringpuffer- Array für empfangene	UART Daten (UART-Puffer)
-//extern volatile unsigned char UART_RxCount ;								// Zählervariable für UART-Puffer
+//extern volatile unsigned char UART_RXBuffer[UART_BUFFER_SIZE];				// Ringpuffer- Array fÃ¼r empfangene	UART Daten (UART-Puffer)
+//extern volatile unsigned char UART_RxCount ;								// ZÃ¤hlervariable fÃ¼r UART-Puffer
 
 // functions & procedures delaration
 extern void init_uart();													// Funktion in RS232.c zum Initalisieren und Aktivieren des UART mit oben eingestellten Werten
